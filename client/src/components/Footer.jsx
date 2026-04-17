7d154c0387b07c052b533902ea580ba12b1dbd7a
Wathsika <182477712+Wathsika@users.noreply.github.com>
2026-04-18 00:28:04 +0530

    footer fix

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Heart, GitHub, Mail, Phone } from "lucide-react";
+import { Heart, Github, Mail, Phone } from "lucide-react";
 
 const Footer = () => {
   return (
